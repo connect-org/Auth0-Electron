4a747989075da56fa7b820de80a3051d38357bd8
Elijah629 <EliOzcan@outlook.com>
2022-10-26 07:10:02 -0700

    Rewrite

--- auth0.ts
+++ auth0.ts
@@ -1,10 +1,35 @@
-import { BrowserWindow, ipcMain, app } from "electron";
+import { BrowserWindow, app } from "electron";
 import * as OS from "os";
 import * as keytar from "keytar";
 import jwtDecode from "jwt-decode";
-import axios from "axios";
+import fetch from "node-fetch";
 
-interface AuthorizationConfiguration {
+/**
+ * @error AuthError
+ * @desc Error class for anything related to Auth
+ */
+
+export class AuthError extends Error {
+	constructor(message?: string) {
+		super(message);
+		this.name = "AuthError";
+	}
+}
+
+/**
+ * @interface User
+ * @desc Interface for user tokens and profile object
+ */
+
+export interface User {
+	profile?: unknown,
+	accessToken?: string,
+	refreshToken?: string
+}
+
+const user: User = {};
+
+export interface ClientOptions {
 	keytar: {
 		service: string
 	},
@@ -14,146 +39,137 @@ interface AuthorizationConfiguration {
 		scopes?: Array<string>,
 		audience?: string
 	},
-	authorizationWindow?: Electron.BrowserWindowConstructorOptions,
-	preload?(authWindow: BrowserWindow): void,
-	onAuthorized(user?: AuthorizedUser): void,
-	calls: {
-		getUser: string,
-		logout: string
+	window: Electron.BrowserWindowConstructorOptions
+}
+
+const DefaultClientOptions = {
+	keytar: { },
+	auth0: { scopes: ["openid", "profile", "offline_access"] }
+};
+
+export class Client {
+	private options: ClientOptions;
+	private authenticated: boolean;
+
+	constructor(options: ClientOptions) {
+		this.options = {...DefaultClientOptions, ...options};
+		this.authenticated = false;
 	}
-}
 
-interface AuthorizedUser {
-	profile?: object,
-	accessToken?: string,
-	refreshToken?: string
-}
+	private async refreshTokens(): Promise<boolean> {
+		const refreshToken = await keytar.getPassword(this.options.keytar.service, OS.userInfo().username);
+	
+		if (refreshToken) {	
+			try {
+				const response = await fetch(`https://${this.options.auth0.domain}/oauth/token`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/x-www-form-urlencoded"
+					},
+					body: new URLSearchParams({
+						grant_type: "refresh_token",
+						client_id: this.options.auth0.clientId,
+						refresh_token: refreshToken
+					})
+				});
 
-const user: AuthorizedUser = {};
-let config: AuthorizationConfiguration;
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+				const data: Record<any, any> = await response.json();
 
-async function refreshTokens() {
-	const refreshToken = await keytar.getPassword(config.keytar.service, OS.userInfo().username);
-
-	if (refreshToken) {
-		const refreshOptions = {
-			method: "POST",
-			url: `https://${config.auth0.domain}/oauth/token`,
-			headers: {
-				"content-type": "application/json"
-			},
-			data: {
-				grant_type: "refresh_token",
-				client_id: config.auth0.clientId,
-				refresh_token: refreshToken
+				user.accessToken = data.access_token;
+				user.profile = jwtDecode(data.id_token);
+				return true;
+			} catch (error) {
+				await this.keytarLogout();
+				throw error;
 			}
-		};
+		} else {
+			return false;
+		}
+	}
+	
+	private async keytarLogout() {
+		await keytar.deletePassword(this.options.keytar.service, OS.userInfo().username);
+	}
 
+	private async loadTokens(callbackURL: URL) {
 		try {
-			const response = await axios(refreshOptions);
-			user.accessToken = response.data.access_token;
-			user.profile = jwtDecode(response.data.id_token);
+			const query = callbackURL.searchParams;
+
+			const response = await fetch(`https://${this.options.auth0.domain}/oauth/token`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded"
+				},
+				body: new URLSearchParams({
+					grant_type: "authorization_code",
+					client_id: this.options.auth0.clientId,
+					// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+					code: query.get("code")!,
+					redirect_uri: "https://localhost/callback",
+				})
+			});
+
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			const data: Record<any, any> = await response.json();
+
+			user.accessToken = data.access_token;
+			user.profile = jwtDecode(data.id_token);
+			user.refreshToken = data.refresh_token;
+			if (user.refreshToken) {
+				await keytar.setPassword(this.options.keytar.service, OS.userInfo().username, user.refreshToken);
+			}
 		} catch (error) {
-			await logout();
+			await this.keytarLogout();
 			throw error;
 		}
-	} else {
-		throw new Error("No available refresh token.");
 	}
-}
 
-async function loadTokens(callbackURL: URL) {
-	const query = callbackURL.searchParams;
+	async login(): Promise<User> {
+		if (this.refreshTokens()) {
+			return user;
+		} else {
+			const authURL = new URL(`https://${this.options.auth0.domain}/authorize`);
 
-	const options = {
-		method: "POST",
-		url: `https://${config.auth0.domain}/oauth/token`,
-		headers: {"content-type": "application/x-www-form-urlencoded"},
-		data: new URLSearchParams({
-			grant_type: "authorization_code",
-			client_id: config.auth0.clientId,
-			code: query.get("code")!,
-			redirect_uri: "https://localhost/callback",
-		}),
-	};
+			const authURLQuery: Record<string, string> = {
+				scope: this.options.auth0.scopes.join(" "),
+				response_type: "code",
+				client_id: this.options.auth0.clientId,
+				redirect_uri: "https://localhost/callback"
+			};
 
-	try {
-		const response = await axios(options);
-		user.accessToken = response.data.access_token;
-		user.profile = jwtDecode(response.data.id_token);
-		user.refreshToken = response.data.refresh_token;
-		if (user.refreshToken) {
-			await keytar.setPassword(config.keytar.service, OS.userInfo().username, user.refreshToken);
+			if (this.options.auth0.audience) {
+				authURLQuery.audience = this.options.auth0.audience;
+			}
+			
+			Object.keys(authURLQuery).forEach(key => {
+				authURL.searchParams.append(key, authURLQuery[key]);
+			});
+
+			const authWindow = new BrowserWindow(this.options.window);
+			authWindow.loadURL(authURL.href);
+
+			const webRequest = authWindow.webContents.session.webRequest;
+
+			// When the auth window redirects to the callback url
+			webRequest.onBeforeRequest({ urls: ["connect://auth/callback*"] }, async ({ url }) => {
+				await this.loadTokens(new URL(url));
+				authWindow.close();
+				return user;
+			});
 		}
-	} catch (error) {
-		await logout();
-		throw error;
-	}
-}
-
-async function logout() {
-	await keytar.deletePassword(config.keytar.service, OS.userInfo().username);
-}
-
-async function authorize(options: AuthorizationConfiguration) {
-	config = options;
-	config.auth0.scopes ??= ["openid", "profile", "offline_access"];
-	config.authorizationWindow ??= {
-		width: 470,
-		height: 680
-	};
-	const authURL = new URL(`https://${config.auth0.domain}/authorize`);
-	const authURLParams = new URLSearchParams({
-		scope: config.auth0.scopes.join(" "),
-		response_type: "code",
-		client_id: config.auth0.clientId,
-		redirect_uri: "https://localhost/callback",
-	});
-
-	if (config.auth0.audience) {
-		authURLParams.set("audience", config.auth0.audience);
-	}
-
-	Array.from(authURLParams.entries()).forEach(entry => {
-		authURL.searchParams.set(entry[0], entry[1]);
-	});
-
-	try {
-		await refreshTokens();
-		config.onAuthorized(user);
-	} catch (err) {
-		const authWindow = new BrowserWindow(config.authorizationWindow);
-		authWindow.loadURL(authURL.toString());
 		
-		config.preload && config.preload(authWindow);
-
-		const webRequest = authWindow.webContents.session.webRequest;
-
-		webRequest.onBeforeRequest({ urls: ["https://localhost/callback*"] }, async ({ url }) => {
-			await loadTokens(new URL(url));
-			config.onAuthorized();
-
-			authWindow.close();
-		});
-
-		// authWindow.on("authenticated", () => {
-			// authWindow.close();
-		// });
 	}
-	ipcMain.handle(config.calls.getUser, () => user);
-	ipcMain.on(config.calls.logout, () => {
+
+	async logout() {
+		if (!this.authenticated) {
+			throw new AuthError("Logout: Attemped to logout while client is not authenticated");
+		}
+
 		BrowserWindow.getAllWindows().forEach(window => window.close());
-		const logoutWindow = new BrowserWindow({ show: false });
-
-		logoutWindow.loadURL(`https://${config.auth0.domain}/v2/logout`);
-
-		logoutWindow.on("ready-to-show", async () => {
-			await logout();
-			logoutWindow.close();
-			app.relaunch();
-			app.exit();
-		});
-	});
+		await fetch(`https://${this.options.auth0.domain}/v2/logout`);
+		await this.keytarLogout();
+		app.relaunch();
+		app.exit();
+	}
 }
-
-export default authorize;
